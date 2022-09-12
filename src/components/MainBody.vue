1c9b7057e922dad07dede93811dfbf0e81bc401b
Alaikum <carangiadamo@gmail.com>
2022-09-13 01:02:22 +0200

    milestone 3

--- src/components/MainBody.vue
+++ src/components/MainBody.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <input v-model="search" type="text" placeholder="Certa il tuo film">
+    <input v-model="search" type="text" placeholder="Certa il tuo film" @keyup.enter="fetchMovies">
     <button @click="fetchMovies">Cerca</button>
+     
+
     <!-- <select v-model="language" name="" id="">
       <option value=""></option>
       <option value="it">Italiano</option>
@@ -26,8 +28,18 @@
       </figure>
 
       <p>
-        Voto: {{el.vote_average}}
+        Voto:
+        <span v-for="star in voteStar(el)" :key="star">
+          <font-awesome-icon icon="fa-solid fa-star" />
+        </span>
+        <span v-for="(star) in noVoteStar(el)" :key="star+el.original_title">
+          <font-awesome-icon icon="fa-regular fa-star" />
+        </span>
+
       </p>
+      <figure>
+        <img :src="posterPhoto(el)" alt="Poster Not Found">
+      </figure>
     </div>
 
     <div class="main" v-for="(el,i) in seriesList" :key="el.name+i">
@@ -42,10 +54,21 @@
       </p>
       <figure>
         <img :src="`https://flagcdn.com/32x24/${el.original_language}.png`" alt="">
+
       </figure>
       <p>
-        Voto Serie: {{el.vote_average}}
+        Voto Serie:
+        <span v-for="star in voteStar(el)" :key="star">
+          <font-awesome-icon icon="fa-solid fa-star" />
+        </span>
+        <span v-for="star in noVoteStar(el)" :key="star+el.original_name">
+          <font-awesome-icon icon="fa-regular fa-star" />
+        </span>
+
       </p>
+      <figure>
+        <img :src="posterPhoto(el)" alt="Poster Not Found">
+      </figure>
     </div>
   </div>
 </template>
@@ -58,6 +81,10 @@ export default {
       filmList: [],
       search: '',
       seriesList: [],
+      imgFlag: '',
+
+
+
 
 
     }
@@ -78,7 +105,7 @@ export default {
           // console.log(res.data.results);
           this.filmList = res.data.results;
           this.translateFlags(this.filmList)
-         
+
         })
         .catch((err) => {
           console.log(err)
@@ -106,28 +133,71 @@ export default {
       this.search = ''
 
     },
-    translateFlags(el){
+    // getFlags(el){
+    //   axios.get(`https://flagcdn.com/32x24/${el.original_language}.png`)
+    //     .then((res) => {
+    //       console.log(el,res.data);
+
+    //       // this.seriesList = res.data.results;
+    //       // this.translateFlags(this.seriesList)
+    //     })
+    //     .catch((err) => {
+    //       console.log(err)
+    //     })
+    //     .finally(() => {
+    //       console.log('fine flags')
+
+    //     })
+
+    // },
+
+    posterPhoto(el) {
+      if (el.poster_path === null) { return }
+
+      return `https://image.tmdb.org/t/p/w92/${el.poster_path}`
+    },
+    voteStar(el) {
+      // console.log(el.vote_average)
+      let votoStelle = Math.ceil(el.vote_average / 2)
+      // console.log(votoStelle)
+      return votoStelle
+    },
+    noVoteStar(el) {
+
+      let noVotoStelle = Math.ceil(4 - (el.vote_average / 2))
+      // console.log(votoStelle)
+
+
+      return noVotoStelle
+    },
+    translateFlags(el) {
       console.log(el)
       el.forEach(el => {
-            if (el.original_language == 'en') {
-              el.original_language = 'gb'
-            }
-            if (el.original_language == 'hi') {
-              el.original_language = 'in'
-            }
-            if (el.original_language == 'ja') {
-              el.original_language = 'jp'
-            }
-            if (el.original_language == 'fa') {
-              el.original_language = 'ir'
-            }
-            if (el.original_language == 'ko') {
-              el.original_language = 'kr'
-            }
-            if (el.original_language == 'cs') {
-              el.original_language = 'cz'
-            }
-          });
+        if (el.original_language == 'en') {
+          el.original_language = 'gb'
+        }
+        if (el.original_language == 'hi') {
+          el.original_language = 'in'
+        }
+        if (el.original_language == 'ja') {
+          el.original_language = 'jp'
+        }
+        if (el.original_language == 'fa') {
+          el.original_language = 'ir'
+        }
+        if (el.original_language == 'ko') {
+          el.original_language = 'kr'
+        }
+        if (el.original_language == 'cs') {
+          el.original_language = 'cz'
+        }
+        if (el.original_language == 'zh') {
+          el.original_language = 'cn'
+        }
+        if (el.original_language == 'uk') {
+          el.original_language = 'ua'
+        }
+      });
 
     }
   }
